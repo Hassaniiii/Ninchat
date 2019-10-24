0ed41eb8ab5b4a6673c51402a5da09915a358380
Hassan Shahbazi <h-shahbazi@hotmail.com>
2019-10-24 23:52:46 +0300

    Initial Commit

---
+++ Ninchat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ninchat
+//
+//  Created by Hassaniiii on 10/24/19.
+//  Copyright © 2019 Hassaniiii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
